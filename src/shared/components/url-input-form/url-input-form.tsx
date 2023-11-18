e67d2a5a9840dc22c9486c39e9e5e21be6857a50
Faraz Khan <farazkhanaa2555@gmail.com>
2023-11-19 00:43:51 +0500

    feat: Backend Integration with Rapid API

--- src/shared/components/url-input-form/url-input-form.tsx
+++ src/shared/components/url-input-form/url-input-form.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import EnterIcon from "@/shared/icons/enter-icon";
 import LinkIcon from "@/shared/icons/link-icon";
 import CustomButton from "../custom-button/custom-button";
 import { CustomButtonTypes } from "../custom-button/custom-button.types";
 import { CustomInputField } from "../custom-input-field/custom-input-field";
 import { CustomInputFieldType } from "../custom-input-field/custom-input.types";
+import ArticleSummaryService from "@/shared/services/article-summary-service";
 
 const UrlInputForm = () => {
+  const [articleUrl, setArticleUrl] = useState<string>("");
+
+  const submitUrl = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const summaryData =
+      await ArticleSummaryService.getArticleSummary(articleUrl);
+    // log summaryData console can be removed after
+    console.log(summaryData);
+  };
+
   return (
-    <form className="relative max-w-xl w-full mx-auto mb-3">
+    <form
+      className="relative max-w-xl w-full mx-auto mb-3"
+      onSubmit={submitUrl}
+    >
       <LinkIcon
         className="absolute left-2 top-3"
         width={20}
@@ -20,6 +35,7 @@ const UrlInputForm = () => {
         type={CustomInputFieldType.URL}
         placeholder="Enter Your URL"
         className="input-field"
+        onChange={(e) => setArticleUrl(e.target.value)}
       />
 
       <CustomButton
